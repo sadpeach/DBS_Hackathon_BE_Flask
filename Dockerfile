fe9445a0392b5f0b1ca0c341fdf8ad8a4ef921a6
huang Jiayi <guangdongjiayi@gmail.com>
2022-07-21 23:23:58 +0800

    updated

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ ENV PYTHONUNBUFFERED True
 COPY . /app
 WORKDIR /app
 RUN pip3 --no-cache-dir install -r requirements.txt   
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
 
--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=True)
